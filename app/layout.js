2307d97373a6c0b78942f3b1136d1659c168950a
JustEricGR <eric.ratiu@gmail.com>
2026-07-13 21:56:47 +0300

    proiectBun..

--- app/layout.js
+++ app/layout.js
@@ -14,10 +14,3 @@ export default function RootLayout({ children }) {
   description: 'Creat cu Next.js si Supabase',
 }
 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="ro">
-      <body style={{ margin: 0, padding: 0 }}>{children}</body>
-    </html>
-  )
-}
